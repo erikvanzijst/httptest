e405c5b09e8b170a3bbb28968228efb3baa60fe8
Brodie Rao <brodie@sf.io>
2015-08-31 00:53:03 -0700

    tests: allow the tests module to be run as a script

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 """Tests for httptest"""
 
 import doctest
+import unittest
 
 import httptest
 
@@ -8,3 +9,6 @@ def load_tests(loader, tests, ignore):
     """Add httptest's doctests to the list of unit tests"""
     tests.addTests(doctest.DocTestSuite(httptest))
     return tests
+
+if __name__ == '__main__':
+    unittest.main()
